3b52078d698b989d5a67978f3548e297cb8a0229
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-10-11 16:46:12 -0500

    Update anonymous_ftp.py

--- concepts/python/anonymous_ftp.py
+++ concepts/python/anonymous_ftp.py
@@ -1,15 +1,35 @@
+import argparse
 from ftplib import FTP
 
 
-def ftp_handler(host, port):
-    print(f'ftp_handler called for port {port}')
+def main(args):
+    print(f'ftp_handler called for port {args.port}')
+    ftp = FTP()
     try:
-        ftp = FTP(host, port)
-        ftp.login()
+        ftp.connect(args.host, args.port, timeout=args.timeout)
+        ftp.login(user='anonymous', passwd='me@me.com')
+        ftp.set_pasv(True)
         files = ftp.nlst()
         ftp.quit()
         print('Anonymous login successful. Here are the files on the ftp server:')
         for file in files:
             print(file)
-    except Exception:
-        pass
+    except Exception as e:
+        print(f'FTP error: {e}', file=sys.stderr)
+    finally:
+        try:
+            ftp.quit()
+        except Exception:
+            try:
+                ftp.close()
+            except Exception:
+                pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='A script to log into ftp with anonymous user and show the contents of all files')
+    parser.add_argument('host', help='The host name of the attacker machine.')
+    parser.add_argument('--port', type=int, default=21, help='The port of the attacker machine.')
+    parser.add_argument('--timeout', type=int, default=10, help='The default timeout in seconds to connect.')
+    args = parser.parse_args()
+    main(args)
