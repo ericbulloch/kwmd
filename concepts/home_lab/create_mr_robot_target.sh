c73348ccc2202014b47a999757c03b1ba8601d2b
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-10-15 12:52:38 -0500

    Update create_mr_robot_target.sh

--- concepts/home_lab/create_mr_robot_target.sh
+++ concepts/home_lab/create_mr_robot_target.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+OVA_PATH="/path/to/MrRobot.ova"
+VM_NAME="MrRobot"
+HOSTONLY_IF="vboxnet0"
+
+# Import the VM
+vboxmanage import "$OVA_PATH" --vsys 0 --vmname "$VM_NAME"
+
+# Remove internet access
+vboxmanage modifyvm "$VM_NAME" --nic1 hostonly --hostonlyadapter1 "$HOSTONLY_IF"
