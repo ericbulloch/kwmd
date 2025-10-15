ee808289b1785441e455f5e262200cd3b34c47ea
Eric Bulloch <ericbulloch@users.noreply.github.com>
2025-10-15 12:51:24 -0500

    Update create_network.sh

--- concepts/home_lab/create_network.sh
+++ concepts/home_lab/create_network.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
 
+# Create a new host-only network
+vboxmanage hostonlyif create
+
+# Get the name of the newly created interface
+IFACE=$(vboxmanage list hostonlyifs | grep -B1 "IPAddress: 192.168.56.1" | head -n1 | awk -F: '{print $2}' | xargs)
+
+# Configure the interface with a custom subnet
+vboxmanage hostonlyif ipconfig "$IFACE" --ip 192.168.56.1 --netmask 255.255.255.0
+
+# Disable DHCP to prevent host access
+vboxmanage dhcpserver remove --ifname "$IFACE"
